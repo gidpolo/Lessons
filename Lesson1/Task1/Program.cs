d914636720633a0d28a71b8399ae441bf671a5b4
Gidpolo <gidpolo@mail.ru>
2023-01-23 23:50:04 +0300

    Task1 comleted

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите два числа: ");
+string A = Console.ReadLine() ?? "";
+string B = Console.ReadLine() ?? "";
+int A_int = Convert.ToInt32(A);
+int B_int = Convert.ToInt32(B);
+if (A_int > B_int)
+{
+  System.Console.WriteLine($"Число {A} больше числа {B}");
+}
+else
+{
+    System.Console.WriteLine($"Число {B} больше числа {A}");
+}
