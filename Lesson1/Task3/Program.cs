e99d2c1a6f6320e24e885ff42dd264f33ae92f31
Gidpolo <gidpolo@mail.ru>
2023-01-24 00:19:19 +0300

    Task3 completed

--- Lesson1/Task3/Program.cs
+++ Lesson1/Task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число, чтобы узнать чётное ли оно: ");
+string A = Console.ReadLine() ?? "";
+int A_int = Convert.ToInt32(A);
+if (A_int % 2 == 0)
+{
+    System.Console.WriteLine("Y");
+}
+else
+{
+    System.Console.WriteLine("N");
+}
