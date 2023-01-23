ff5ce03d5189c2845e7c786e750c4e66f2b06edb
Gidpolo <gidpolo@mail.ru>
2023-01-24 00:44:00 +0300

    Task4 completed

--- Lesson1/Task4/Program.cs
+++ Lesson1/Task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число, до которого будут выведены все чётные числа: ");
+string N = Console.ReadLine() ?? "";
+int N_int = Convert.ToInt32(N);
+
+ if (N_int==0)
+{
+    System.Console.WriteLine("Вы ввели 0");
+}
+
+if (N_int == 1)
+{
+    System.Console.WriteLine("До 1 нет чётных чисел");
+}
+
+for (int i = 2; i <= N_int; i+=2)
+{
+    System.Console.Write($"{i} ");
+
+}
