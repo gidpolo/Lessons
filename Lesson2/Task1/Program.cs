ada914512d0ed66b1edda37551b361e8365a878e
Gidpolo <gidpolo@mail.ru>
2023-01-24 01:09:45 +0300

    Stop working

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите трехзначное число, чтобы узнать вторую цифру этого числа: ");
+string N = Console.ReadLine() ?? "";
+int N_int = Convert.ToInt32(N);
+int i=0;
+while (true)
+{
+
+do
+{
+    N_int %= 10;
+    i++;
+} while (N_int!=0);
+ if (i>3 || i<3)
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число");
+        break;
+    }
+
+}
+// Ушёл в дебри, закончу позже
