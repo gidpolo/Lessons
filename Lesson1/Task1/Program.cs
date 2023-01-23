af7ffb36cef1872d192613213a9632d22f438172
Gidpolo <gidpolo@mail.ru>
2023-01-24 00:13:46 +0300

    Task2 completed

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,13 +1,20 @@
 ﻿System.Console.WriteLine("Введите два числа: ");
+System.Console.WriteLine("Первое: ");
+
 string A = Console.ReadLine() ?? "";
+
+System.Console.WriteLine("Первое: ");
+
 string B = Console.ReadLine() ?? "";
+
 int A_int = Convert.ToInt32(A);
 int B_int = Convert.ToInt32(B);
+
 if (A_int > B_int)
 {
   System.Console.WriteLine($"Число {A} больше числа {B}");
 }
 else
 {
-    System.Console.WriteLine($"Число {B} больше числа {A}");
+    System.Console.WriteLine($"Число {A} больше числа {B}");
 }
--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿System.Console.WriteLine("Введите 3 числа, для нахождения максимального из них");
+System.Console.WriteLine("Первое: ");
+
+string A = Console.ReadLine() ?? "";
+
+System.Console.WriteLine("Второе:");
+
+string B = Console.ReadLine() ?? "";
+
+System.Console.WriteLine("Третье:");
+
+string C = Console.ReadLine() ?? "";
+
+int A_int = Convert.ToInt32(A);
+int B_int = Convert.ToInt32(B);
+int C_int = Convert.ToInt32(C);
+
+if (A_int > B_int && A_int > C_int)
+{
+    System.Console.WriteLine($"Число {A_int} самое большое");
+}
+else if (B_int > A_int && B_int > C_int)
+{
+    System.Console.WriteLine($"Число {B_int} самое большое");
+}
+else
+{
+    System.Console.WriteLine($"Число {C_int} самое большое");
+}
