08d1a1626fe1357ce8a085595b7a4d96fbb42a8b
Gidpolo <gidpolo@mail.ru>
2023-01-25 00:45:30 +0300

    Пытаюсь сделать проверку на введеное число / не готово

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,20 +1,35 @@
 ﻿System.Console.WriteLine("Введите трехзначное число, чтобы узнать вторую цифру этого числа: ");
-string N = Console.ReadLine() ?? "";
-int N_int = Convert.ToInt32(N);
-int i=0;
-while (true)
-{
 
+static string UserRead()
+{
+   string N = Console.ReadLine() ?? "";
+   return N;
+}
+int i = 0;
+int i1 = 10;
+string[] a = Array.Empty<string>();
 do
 {
-    N_int %= 10;
-    i++;
-} while (N_int!=0);
- if (i>3 || i<3)
+    string N = UserRead();
+    while (true)
     {
-        System.Console.WriteLine("Вы ввели не трехзначное число");
+       int N1 = N %= i1;
+       a[0+i] = a[0+i] + N;
+       i++;
+       i1+=10;
+       if (N1 == null) 
+       int N2 = a.Length;
+       if (N2 > 3 || N2 < 3)
+       {
+        System.Console.WriteLine("Вы ввели не трёхзначное число, попробуйте снова: ");
+        continue;
+       }
+       else
+       {
         break;
+       }
     }
-
-}
+    break;
+} while (true);
+    
 // Ушёл в дебри, закончу позже
