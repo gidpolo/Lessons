913b0d8dc465204e68f240e5f15ee1849f6a1a74
Gidpolo <gidpolo@mail.ru>
2023-01-26 23:55:08 +0300

    task2 completed

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,35 +1,22 @@
 ﻿System.Console.WriteLine("Введите трехзначное число, чтобы узнать вторую цифру этого числа: ");
 
-static string UserRead()
+int UserRead()
 {
-   string N = Console.ReadLine() ?? "";
-   return N;
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
 }
-int i = 0;
-int i1 = 10;
-string[] a = Array.Empty<string>();
-do
+
+while (true)
 {
-    string N = UserRead();
-    while (true)
+    int N = UserRead();
+    if (N < 100 || N > 999)
     {
-       int N1 = N %= i1;
-       a[0+i] = a[0+i] + N;
-       i++;
-       i1+=10;
-       if (N1 == null) 
-       int N2 = a.Length;
-       if (N2 > 3 || N2 < 3)
-       {
         System.Console.WriteLine("Вы ввели не трёхзначное число, попробуйте снова: ");
         continue;
-       }
-       else
-       {
-        break;
-       }
     }
-    break;
-} while (true);
     
-// Ушёл в дебри, закончу позже
+    N= N/10 % 10;
+    Console.WriteLine($"Вторая цифра введенного вами числа: {N}");
+    break;
+}
