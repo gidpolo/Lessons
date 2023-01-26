7f12d9b9781f5ad5824011c46b06f10c071688fd
Gidpolo <gidpolo@mail.ru>
2023-01-27 01:13:03 +0300

    Task 3 completed

--- Lesson2/Task3/Program.cs
+++ Lesson2/Task3/Program.cs
@@ -1,2 +1,32 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Введите число соответствующее дню недели, чтобы узнать выходной ли этот день: ");
+
+int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+
+while (true)
+{
+    int N = UserRead();
+    if (N == 0 || N > 7)
+    {
+        System.Console.WriteLine("Вы ввели цифру не соответствующую дню недели, попробуйте снова: ");
+        continue;
+    }
+    switch (N)
+    {
+        case 1: Console.WriteLine ("Нет"); break;    
+        case 2: Console.WriteLine ("Нет"); break; 
+        case 3: Console.WriteLine ("Нет"); break; 
+        case 4: Console.WriteLine ("Нет"); break; 
+        case 5: Console.WriteLine ("Нет"); break; 
+        case 6: Console.WriteLine ("Да"); break; 
+        case 7: Console.WriteLine ("Да"); break;     
+    }
+    break;
+}
+
+
+
