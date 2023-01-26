691caae1b23f93c409115c5e6c7dcef14805d010
Gidpolo <gidpolo@mail.ru>
2023-01-27 01:04:04 +0300

    task 3 completed

--- Lesson2/Task3/Program.cs
+++ Lesson2/Task3/Program.cs
@@ -1,4 +1,4 @@
-﻿System.Console.WriteLine("Введите число, чтобы узнать третью цифру этого числа: ");
+﻿System.Console.WriteLine("Введите число, которое соответствует дню недели, чтобы проверить является ли он выходным: ");
 
 int UserRead()
 {
@@ -10,16 +10,22 @@ int UserRead()
 while (true)
 {
     int N = UserRead();
-    if (N < 100)
+    if (N == 0 || N > 7)
     {
-        System.Console.WriteLine("Вы ввели число у которого нет третьей цифры, попробуйте снова: ");
+        System.Console.WriteLine("Вы ввели число не соответствующее дню недели, попробуйте снова: ");
         continue;
     }
 
-    while (N>1000)
+    switch (N)
     {
-        N/=10;
+        case 1: Console.WriteLine("Нет"); break;
+        case 2: Console.WriteLine("Нет"); break;
+        case 3: Console.WriteLine("Нет"); break;
+        case 4: Console.WriteLine("Нет"); break;
+        case 5: Console.WriteLine("Нет"); break;
+        case 6: Console.WriteLine("Да"); break;
+        case 7: Console.WriteLine("Да"); break;
     }
-    Console.WriteLine($"Третья цифра введенного вами числа: {N%10}");
+
     break;
 }
