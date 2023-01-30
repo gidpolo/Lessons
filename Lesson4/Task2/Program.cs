a020dfaf43a3e3608d73c1781b29c1ec09ba8cd1
Valentin <gidpolo@mail.ru>
2023-01-30 19:58:19 +0300

    Task2 in lesson4 completed

--- Lesson4/Task2/Program.cs
+++ Lesson4/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+Console.WriteLine("Введите число, чтобы узнать сумму его цифр:");
+int N = UserRead();
+int N1 = 0;
+while (N != 0)
+{
+    N1 += N%10;
+    N /=10;
+}
+Console.WriteLine(N1);
