2024638d8a509a295fcdc88f5177635140fe6341
Valentin <gidpolo@mail.ru>
2023-01-30 18:25:40 +0300

    Upgrated Task1 and completed Task2

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int UserRead()
+﻿int UserRead()
 {
     string N = Console.ReadLine() ?? "";
     int N_int = Convert.ToInt32(N);
@@ -19,24 +17,11 @@ int Check(int N)
     return N1;
 }
 
-Console.WriteLine("Введите пятизначное число, чтобы узнать является ли оно палидромом: ");
+Console.WriteLine("Введите число, чтобы узнать является ли оно палидромом: ");
 
 while (true)
 {
     int N = UserRead();
-    int N1 = N;
-    int i = 0;
-    while (N1 != 0)
-    {
-        N1 /= 10;
-        i++;
-    }
-
-    if ((i > 5) || (i < 5))
-    {
-        Console.WriteLine("Вы ввели не пятизначное число, попробуйте снова: ");
-        continue;
-    }
     if (N == Check(N)) Console.WriteLine("Да");
     else Console.WriteLine("Нет");
     break;
--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+
+int[] Coords = new int[6];
+string[] NamesOfCoords = {"A(X)","A(Y)","A(Z)","B(X)","B(Y)","B(Z)"};
+Console.WriteLine("Введите последовательно координаты X,Y,Z точек A и B, для того чтобы узнать расстояние между ними в 3D пространстве:");
+for (int i = 0, j = 0; i < 6 && j < 6; i++, j++)
+{
+    Console.WriteLine($"Введите координату {NamesOfCoords[j]}: ");
+    int N = UserRead();
+    Coords[i] = N;
+}
+double CoordX = Coords[3] - Coords[0];
+double CoordY = Coords[4]- Coords[1];
+double CoordZ = Coords[5] - Coords[2];
+// double b = (3*3 + 2*2)+(6*6 + 1*1) + (8*8 + (-7)*(-7));
+double n = (Math.Sqrt(Math.Pow(CoordX,2) + Math.Pow(CoordY,2) + Math.Pow(CoordZ,2)));
+string result = string.Format("{0:f2}",n);
+Console.WriteLine(result);
