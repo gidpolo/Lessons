e851cc0e9732693945281c7ee01ccdd69dbf49bd
Valentin <gidpolo@mail.ru>
2023-01-30 19:01:26 +0300

    Task3 completed

--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -6,7 +6,7 @@
 }
 
 int[] Coords = new int[6];
-string[] NamesOfCoords = {"A(X)","A(Y)","A(Z)","B(X)","B(Y)","B(Z)"};
+string[] NamesOfCoords = { "A(X)", "A(Y)", "A(Z)", "B(X)", "B(Y)", "B(Z)" };
 Console.WriteLine("Введите последовательно координаты X,Y,Z точек A и B, для того чтобы узнать расстояние между ними в 3D пространстве:");
 for (int i = 0, j = 0; i < 6 && j < 6; i++, j++)
 {
@@ -15,9 +15,9 @@ for (int i = 0, j = 0; i < 6 && j < 6; i++, j++)
     Coords[i] = N;
 }
 double CoordX = Coords[3] - Coords[0];
-double CoordY = Coords[4]- Coords[1];
+double CoordY = Coords[4] - Coords[1];
 double CoordZ = Coords[5] - Coords[2];
 // double b = (3*3 + 2*2)+(6*6 + 1*1) + (8*8 + (-7)*(-7));
-double n = (Math.Sqrt(Math.Pow(CoordX,2) + Math.Pow(CoordY,2) + Math.Pow(CoordZ,2)));
-string result = string.Format("{0:f2}",n);
+double n = (Math.Sqrt(Math.Pow(CoordX, 2) + Math.Pow(CoordY, 2) + Math.Pow(CoordZ, 2)));
+string result = string.Format("{0:f2}", n);
 Console.WriteLine(result);
--- Lesson3/Task3/Program.cs
+++ Lesson3/Task3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+
+Console.WriteLine("Введите число, для того чтобы вывести все кубы чисел от 1 до вашего числа: ");
+int N = UserRead();
+int N1 = N;
+int[] array = new int[N];
+int j = 0;
+Console.Write($"{N} -> ");
+while (N1!=0)
+{
+    array[j]= j + 1;
+    double c = Math.Pow(array[j], 3);
+    j++;
+    N1--;
+    Console.Write($"{c} ");
+}
