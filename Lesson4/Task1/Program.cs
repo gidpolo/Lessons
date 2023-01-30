76da01eb80b598bf03c094f59531258a2959a821
Valentin <gidpolo@mail.ru>
2023-01-30 19:53:03 +0300

    Task1 in Lesson4 completed

--- Lesson4/Task1/Program.cs
+++ Lesson4/Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+Console.WriteLine("Введите числа A и B, чтобы возвести A в степень B:");
+Console.Write("A -> ");
+int A = UserRead();
+int A1 = A;
+Console.Write("B -> ");
+int[] array = new int[UserRead()];
+int k = array.Length - 1;
+while (k != 0)
+{
+    A*=A1;
+    k--;
+}
+Console.WriteLine($"A^B = {A}");
