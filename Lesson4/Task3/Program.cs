76b4c8b1abfdc94428c87a8e71f987fcb64cc566
Gidpolo <gidpolo@mail.ru>
2023-02-02 17:36:08 +0300

    Task 3 in Lesson 4 completed

--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+void FillArray(int N, int X, int Y)
+{
+    int[] array = new int[N];
+    Random rand = new Random();
+    for (int y = 0; y < N; y++)
+    {
+        array[y] = rand.Next(X, Y);
+    }
+    Console.Write("[ ");
+    foreach (var i in array)
+    {
+        Console.Write("{0} ", i);
+    }
+    Console.Write("]");
+}
+while (true)
+{
+    Console.WriteLine("Введите минимальный порог случайных значений: ");
+    int X = UserRead();
+    Console.WriteLine("Введите максимальный порог случайных значений: ");
+    int Y = UserRead();
+    if (X > Y)
+    {
+        Console.WriteLine("Минимальный порог не может быть больше максимального. Попробуйте ещё раз: "); 
+        continue;
+    }
+    Console.WriteLine("Введите количество элементов массива: ");
+    int N = UserRead();
+    FillArray(N, X, Y);
+    break;
+}
+
+
