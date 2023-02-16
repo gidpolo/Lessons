d6ca13cb7af080fc21eb416da6ccf63667dd488c
Gidpolo <gidpolo@mail.ru>
2023-02-16 14:18:45 +0300

    Created new files

---
+++ Lesson7/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+int[] FillArray(int N, int[] array)
+{
+    Random rand = new Random();
+    for (int y = 0; y < N; y++)
+    {
+        array[y] = rand.Next(-100, 100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    foreach (var i in array)
+    {
+        Console.Write("{0} ", i);
+    }
+    Console.Write("]");
+}
