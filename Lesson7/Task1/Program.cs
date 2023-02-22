f0c0847c300b070801e39c093d7adabd194b24ba
Gidpolo <gidpolo@mail.ru>
2023-02-23 02:31:50 +0300

    Task1 in Lesson7 completed

--- Lesson7/Task1/Program.cs
+++ Lesson7/Task1/Program.cs
@@ -4,21 +4,34 @@
     int N_int = Convert.ToInt32(N);
     return N_int;
 }
-int[] FillArray(int N, int[] array)
+double[,] FillArray(int N, int M, double[,] array)
 {
     Random rand = new Random();
-    for (int y = 0; y < N; y++)
+    for (int i = 0; i < N; i++)
     {
-        array[y] = rand.Next(-100, 100);
+        for (int j = 0; j < M; j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-1000, 1000)) / 10;
+        }
     }
     return array;
 }
-void PrintArray(int[] array)
+void PrintArray(double[,] array)
 {
-    Console.Write("[ ");
-    foreach (var i in array)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write("{0} ", i);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2} ", array[i, j]);
+        }
+        Console.WriteLine();
     }
-    Console.Write("]");
 }
+
+Console.WriteLine("Введите количество столбцов:");
+int N = UserRead();
+Console.WriteLine("Введите количество строк:");
+int M = UserRead();
+double[,] array = new double[N, M];
+FillArray(N, M, array);
+PrintArray(array);
