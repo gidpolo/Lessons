e1cce66f910096ac242d5f81ea2eab6a8feb0f79
Gidpolo <gidpolo@mail.ru>
2023-02-27 10:48:42 +0300

    Task2 in Lesson9 completed

--- Lesson9/Task2/Program.cs
+++ Lesson9/Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+int SearchNumbers(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        return sum;
+    }
+    sum=SearchNumbers(M + 1, N, sum)+ M;
+    return sum;
+}
+int M = UserRead();
+int N = UserRead();
+int sum = 0;
+Console.WriteLine(" ");
+Console.WriteLine(SearchNumbers(M, N, sum));
