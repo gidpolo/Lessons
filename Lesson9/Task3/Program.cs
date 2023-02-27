7dfa9a254112d3668c13bc5449ae59e19078c03d
Gidpolo <gidpolo@mail.ru>
2023-02-27 10:59:42 +0300

    Task3 in Lesson9 completed

--- Lesson9/Task3/Program.cs
+++ Lesson9/Task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+        if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+int N = UserRead();
+int M = UserRead();
+Console.WriteLine(" ");
+Console.WriteLine(Akk(N,M));
