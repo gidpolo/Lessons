ac70787f630d9e7cca975f585dcce5675a266b8d
Gidpolo <gidpolo@mail.ru>
2023-02-27 10:16:36 +0300

    Task1 in Lesson9 completed

--- Lesson9/Task1/Program.cs
+++ Lesson9/Task1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿int UserRead()
+{
+    string N = Console.ReadLine() ?? "";
+    int N_int = Convert.ToInt32(N);
+    return N_int;
+}
+
+void SearchNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+    if (M % 2 == 0)
+    {
+        Console.Write(M + " ");
+    }
+    SearchNumbers(M + 1, N);
+}
+int M = UserRead();
+int N = UserRead();
+Console.WriteLine(" ");
+SearchNumbers(M, N);
